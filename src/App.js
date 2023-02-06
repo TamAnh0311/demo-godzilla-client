d8fb7788bbbf4dcfc79ceb5c8a0b5c5c0f9d251e
vtanh <anh.vutam@nttdata.com>
2023-02-06 11:50:49 +0700

    Setting up keycloak

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./utils/keycloak"
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactKeycloakProvider authClient={keycloak} initOptions={{
+        onLoad: "login-required",
+        checkLoginIframe: false,
+      }} >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </ReactKeycloakProvider>
     </div>
   );
 }
---
+++ src/utils/keycloak.js
@@ -0,0 +1,10 @@
+import Keycloak from "keycloak-js";
+const keycloak = new Keycloak({
+  url: "http://localhost:9090/",
+  realm: "DemoGodZilla",
+  clientId: "DemoGodZillaKeycloak",
+  publicClient: true,
+  sslRequired: 'external',
+});
+
+export default keycloak;
